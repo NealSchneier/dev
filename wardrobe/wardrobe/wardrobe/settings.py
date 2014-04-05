8585d3d38a373e0cfde8dc54e9bf547308d314a8
Neal Schneier <neal@Neals-MacBook-Air.local>
2014-04-05 19:44:45 -0400

    stuff

--- wardrobe/wardrobe/wardrobe/settings.py
+++ wardrobe/wardrobe/wardrobe/settings.py
@@ -111,7 +111,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/neal/git/dev/wardrobe/wardrobe/app/template"
+    #"/home/neal/git/dev/wardrobe/wardrobe/app/template"
+    "/Users/neal/git/dev/wardrobe/wardrobe/app/template"
 )
 
 INSTALLED_APPS = (
