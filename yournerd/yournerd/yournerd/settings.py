8229f19d77e73bb98a70ed0dcd9fe48d2fbbe6af
nealSchneier <neal.schneier@gmail.com>
2014-07-03 21:55:19 -0400

    Finance Web Service Start

--- yournerd/yournerd/yournerd/settings.py
+++ yournerd/yournerd/yournerd/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'shownerd',                      # Or path to database file if using sqlite3.
+        'NAME': 'finance',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': 'metsfan',
@@ -126,8 +126,14 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
     'yournerd.shownerd',
+    'webService',
+    'rest_framework',
 )
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGINATE_BY': 10
+}
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
--- yournerd/yournerd/yournerd/urls.py
+++ yournerd/yournerd/yournerd/urls.py
@@ -1,18 +1,25 @@
 from django.conf.urls import patterns, include, url
 from shownerd import views 
 from django.conf import settings
-
+from rest_framework import routers
+from webService import views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+
 
 urlpatterns = patterns('',
 
 #    url(r'^hello/$', views.hello),
 #    url(r'^time/$', views.current_datetime),
 #    url(r'^time/plus/(\d{1,2})/$', views.hours_ahead),
-    url(r'^smartphone/$', views.smartphones),
+#    url(r'^smartphone/$', views.smartphones),
 #    url(r'^users/$', views.users),
     # Examples:
     # url(r'^$', 'yournerd.views.home', name='home'),
@@ -20,7 +27,8 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
